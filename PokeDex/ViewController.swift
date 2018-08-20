836c7338adff8111c480d5936e800fa88635b98c
Alisher <aliwer_dihan@yahoo.com>
2018-08-20 15:05:17 +0300

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by User on 20/08/2018.
+//  Copyright © 2018 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
